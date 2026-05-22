76b087aa8ce0e12a79a28b0bfbf8be4f43265cd4
浩进 朱 <3191955858@qq.com>
2026-05-22 15:17:47 +0800

    邮件收件人缓存类实现。

---
+++ src/main/java/com/jessee/git_remote_repo_listener/cache/impl/EmailRecipientCacherImpl.java
@@ -0,0 +1,78 @@
+package com.jessee.git_remote_repo_listener.cache.impl;
+
+import com.jessee.git_remote_repo_listener.cache.EmailRecipientCacher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static java.lang.String.format;
+
+/** 邮件收件人缓存类实现。*/
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EmailRecipientCacherImpl implements EmailRecipientCacher
+{
+    /** 收件人缓存键。*/
+    private static final String
+    RECIPIENT_CACHE_KEY = "git-remote-repo-analyzer:recipient";
+
+    /** Redis 通用响应式模板。*/
+    private final ReactiveRedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public Mono<Void> addOneRecipientCacher(String name, String address)
+    {
+        return
+        this.redisTemplate.opsForHash()
+            .put(RECIPIENT_CACHE_KEY, name, address)
+            .subscribeOn(Schedulers.boundedElastic())
+            .then();
+    }
+
+    @Override
+    public Mono<String> getRecipientAddress(String name)
+    {
+        return
+        this.redisTemplate.opsForHash()
+            .get(RECIPIENT_CACHE_KEY, name)
+            .switchIfEmpty(
+                Mono.error(
+                    new NoSuchElementException(
+                        format(
+                            "Recipient address of name: %s not exist in key: %s",
+                            name, RECIPIENT_CACHE_KEY
+                        )
+                    )
+                )
+            )
+            .cast(String.class)
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @Override
+    public Mono<List<String>> getAllRecipientAddress()
+    {
+        return
+        this.redisTemplate.opsForHash()
+            .entries(RECIPIENT_CACHE_KEY)
+            .switchIfEmpty(
+                Mono.error(
+                    new NoSuchElementException(
+                        "No recipient address in key: " + RECIPIENT_CACHE_KEY
+                    )
+                )
+            )
+            .map(Map.Entry::getValue)
+            .cast(String.class)
+            .collectList()
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+}

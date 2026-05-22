63390abc17310a370bc7fd230a5b78ce137e1868
浩进 朱 <3191955858@qq.com>
2026-05-22 15:09:19 +0800

    branch_ref_change 表实体类，记录分支引用的变化。

---
+++ src/main/java/com/jessee/git_remote_repo_listener/entity/BranchRefChangeEntity.java
@@ -0,0 +1,40 @@
+package com.jessee.git_remote_repo_listener.entity;
+
+import com.jessee.git_remote_repo_listener.constant.RemoteChangeStaus;
+import com.jessee.git_remote_repo_listener.entity.base.BaseEntity;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+/** branch_ref_change 表实体类，记录分支引用的变化。*/
+@Data
+@Accessors(chain = true)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "branch_ref_change")
+public class BranchRefChangeEntity extends BaseEntity
+{
+    @Column(value = "repository_id")
+    private Long repositoryId;
+
+    @Column(value = "branch_id")
+    private Long branchId;
+
+    @Column(value = "prev_latest_commit_hash")
+    private String prevLatestCommitHash;
+
+    @Column(value = "latest_commit_hash")
+    private String latestCommitHash;
+
+    @Column(value = "change_status")
+    private RemoteChangeStaus changeStaus;
+
+    @Column(value = "analyze_id")
+    private Long analyzeId;
+}

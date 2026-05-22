e9a380d2616fc7b74641faca550c4ddcfe1b9751
浩进 朱 <3191955858@qq.com>
2026-05-22 15:05:27 +0800

    analyze_record 表实体类。

---
+++ src/main/java/com/jessee/git_remote_repo_listener/entity/AnalyzeRecord.java
@@ -0,0 +1,23 @@
+package com.jessee.git_remote_repo_listener.entity;
+
+import com.jessee.git_remote_repo_listener.entity.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+/** analyze_record 表实体类。*/
+@Data
+@Accessors(chain = true)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "analyze_record")
+public class AnalyzeRecord extends BaseEntity
+{
+    @Column(value = "analyze_datetime")
+    private LocalDateTime analyzeDateTime;
+}

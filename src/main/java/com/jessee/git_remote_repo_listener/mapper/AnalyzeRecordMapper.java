55b421768f0c2f957c2b171d62b9db206324bb0f
浩进 朱 <3191955858@qq.com>
2026-05-25 17:25:06 +0800

    分析记录表数据映射接口。
    - 新增标记分析完成操作。

--- src/main/java/com/jessee/git_remote_repo_listener/mapper/AnalyzeRecordMapper.java
+++ src/main/java/com/jessee/git_remote_repo_listener/mapper/AnalyzeRecordMapper.java
@@ -1,10 +1,19 @@
 package com.jessee.git_remote_repo_listener.mapper;
 
 import com.jessee.git_remote_repo_listener.entity.AnalyzeRecord;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 /** 分析记录表数据映射接口。*/
 @Repository
 public interface AnalyzeRecordMapper
-    extends ReactiveCrudRepository<AnalyzeRecord, Long> {}
+    extends ReactiveCrudRepository<AnalyzeRecord, Long>
+{
+    /** 在执行完分析后，标记本次分析完成。*/
+    @Modifying
+    @Query("UPDATE analyze_record SET is_completed = 'Y' WHERE id = :id")
+    Mono<Void> setAnalyzComplete(Long id);
+}

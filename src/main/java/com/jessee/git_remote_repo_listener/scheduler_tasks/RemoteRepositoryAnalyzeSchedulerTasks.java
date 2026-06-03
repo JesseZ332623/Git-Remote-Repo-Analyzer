2f433037b5d8322b49573dfd74c0940f386d3207
Jesse <101250851+JesseZ332623@users.noreply.github.com>
2026-06-03 09:39:36 +0800

    远程仓库分析定时任务服务类。 (#8)
    - 修改 remoteRepoAnalysisTask() 方法，在手动调用 subscribe() 时完整的注册所有回调函数，确保整个响应式流的生命周期可观测。

--- src/main/java/com/jessee/git_remote_repo_listener/scheduler_tasks/RemoteRepositoryAnalyzeSchedulerTasks.java
+++ src/main/java/com/jessee/git_remote_repo_listener/scheduler_tasks/RemoteRepositoryAnalyzeSchedulerTasks.java
@@ -7,6 +7,7 @@ import com.jessee.git_remote_repo_listener.service.AnalyzeReportEmailSender;
 import com.jessee.git_remote_repo_listener.service.RemoteRepositoryAnalyzerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscription;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 /** 远程仓库分析定时任务服务类。*/
 @Slf4j
@@ -23,6 +25,24 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @RequiredArgsConstructor
 public class RemoteRepositoryAnalyzeSchedulerTasks
 {
+    /** 手动 subscribe() 后处理 onError() 信号的回调函数。*/
+    private static final Consumer<? super Throwable>
+    ERROR_CONSMER = (error) ->
+        log.error("Execute remote repository analysis scheduled task failed...", error);
+
+    /** 手动 subscribe() 后处理 onComplete() 信号的回调函数。*/
+    private static final Runnable
+    COMPLETE_CONSUMER = () ->
+        log.info("Execute remote repository analysis scheduled task success!");
+
+    /**
+     * 手动 subscribe() 后马上执行的回调函数，
+     * {@link Subscription} 负责处理背压和取消，在这里暂时用不到。
+     */
+    private static final Consumer<? super Subscription>
+    SUBSCRIPTION_CONSUER = (ignore) ->
+        log.info("Start to execute remote repository analysis scheduled task.");
+
     /** 邮件收件人缓存类接口。*/
     private final EmailRecipientCacher cacher;
 
@@ -42,6 +62,41 @@ public class RemoteRepositoryAnalyzeSchedulerTasks
     /**
      * 向缓存中的每个收件人发送所有仓库的变更报告，
      * 每天早上 9 点 05 分和系下午的 5 点 55 分各执行一次。
+     * 在定时任务场景下，订阅的回调处理处理任务就不是框架代劳了，
+     * 我需要自己手动注册回调函数，具体描述如下所式：
+     *
+     * <table>
+     *     <caption>subscribe() 回调方法说明</caption>
+     *     <thead>
+     *         <tr>
+     *             <th>回调函数接口</th>
+     *             <th>触发时机</th>
+     *             <th>执行次数</th>
+     *         </tr>
+     *    </thead>
+     *    <tbody>
+     *        <tr>
+     *            <td>consumer</td>
+     *                <td>每个数据元素</td>
+     *                <td>0 ~ N 次</td>
+     *        </tr>
+     *        <tr>
+     *            <td>errorConsumer</td>
+     *            <td>发生异常</td>
+     *            <td>0 或 1 次</td>
+     *        </tr>
+     *        <tr>
+     *            <td>completeConsumer</td>
+     *            <td>正常完成</td>
+     *            <td>0 或 1 次</td>
+     *        </tr>
+     *        <tr>
+     *            <td>subscriptionConsumer</td>
+     *            <td>订阅建立时</td>
+     *            <td>0 或 1 次</td>
+     *        </tr>
+     *     </tbody>
+     * </table>
      */
     @Scheduled(
         cron = "0 5 9 * * ?",
@@ -60,6 +115,8 @@ public class RemoteRepositoryAnalyzeSchedulerTasks
                 "The remote warehouse analysis task is currently being executed, " +
                 "skipping this scheduled task."
             );
+
+            return;
         }
 
         Mono.zip(
@@ -85,9 +142,10 @@ public class RemoteRepositoryAnalyzeSchedulerTasks
                             this.analyzeReportEmailSender
                                 .send(recipient, analyzeResult.getAnalyzDateTime(), branchFileChanges)
                         ), maxRecipients
-                ).collectList();
+                )
+                .then();
         })
         .doFinally((ignore) -> this.runningFlag.set(false))
-        .subscribe();
+        .subscribe(null, ERROR_CONSMER, COMPLETE_CONSUMER, SUBSCRIPTION_CONSUER);
     }
 }
